714b800cdd4c6e4a2475df0e30a68381968669be
Ivan Frittelli <ivan.frittelli@gmail.com>
2025-12-16 18:07:46 +0100

    Creazione griglia coarse

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <boost/geometry/util/math.hpp>
 #include <deal.II/base/function.h>
 #include <deal.II/base/function_lib.h>
 #include <deal.II/base/function_parser.h>
@@ -21,6 +22,7 @@
 #include <deal.II/lac/sparse_direct.h>
 #include <deal.II/lac/vector.h>
 #include <deal.II/lac/arpack_solver.h>
+#include <deal.II/lac/linear_operator.h>
 
 #include <deal.II/numerics/data_out.h>
 #include <deal.II/numerics/error_estimator.h>
@@ -57,6 +59,7 @@ struct PoissonParameters
       prm.add_parameter("Neumann boundary expression", neumann_expression);
       prm.add_parameter("Neumann boundary ids", neumann_boundary_ids);
       prm.add_parameter("Number of eigenvalues", n_of_eigenvalues);
+      prm.add_parameter("Number of V-cycles", n_v_cycles);
     }
     prm.leave_subsection();
 
@@ -89,6 +92,7 @@ struct PoissonParameters
   unsigned int fe_degree                 = 1;
   unsigned int initial_refinement        = 3;
   unsigned int n_cycles                  = 1;
+  unsigned int n_v_cycles                = 1;
 
   unsigned int n_of_eigenvalues          = 1;
 
@@ -107,7 +111,224 @@ struct PoissonParameters
 };
 
 
+class Graph {
+  public:
+    Graph();
 
+    void add_point(double x, double y, double z);
+    void add_point(Point<3> point);
+
+    void add_cell(int node1, int node2);
+
+    int get_number_of_points();
+    int get_number_of_cells();
+
+    Triangulation<1,3> create_graph_triangulation();
+
+    Graph get_coarser_graph();
+
+  private:
+    std::vector<Point<3>> points;
+    std::vector<CellData<1>> cells;
+
+    std::vector<std::vector<int>> adiacency;
+
+};
+
+Graph::Graph(){
+
+}
+
+void
+Graph::add_point(double x, double y, double z) {
+  points.push_back(Point<3>(x, y,z));
+  adiacency.push_back(std::vector<int>());
+}
+
+void
+Graph::add_point(Point<3> point) {
+  points.push_back(point);
+  adiacency.push_back(std::vector<int>());
+}
+
+void
+Graph::add_cell(int node1, int node2) {
+  CellData<1> cell;
+
+  cell.vertices[0] = node1;
+  cell.vertices[1] = node2;
+
+  adiacency[node1].push_back(cells.size());
+  adiacency[node2].push_back(cells.size());
+
+  cells.push_back(cell);
+} 
+
+
+Triangulation<1, 3>
+Graph::create_graph_triangulation() {
+  Triangulation<1, 3> result;
+
+  result.create_triangulation (points, cells, SubCellData());
+
+  return result;
+}
+
+int 
+Graph::get_number_of_points() {
+  return points.size();
+}
+
+int
+Graph::get_number_of_cells() {
+  return cells.size();
+}
+
+Graph
+Graph::get_coarser_graph() {
+  Graph result;
+
+  std::vector<float> lengths;
+
+  for (const auto & cell : cells) {
+    lengths.push_back((points[cell.vertices[1]] - points[cell.vertices[0]]).norm_square());
+  }
+
+  std::sort(lengths.begin(), lengths.end(), [](const float & a, const float & b) {
+    return a < b;
+  });
+
+  int n_cells = cells.size();
+
+  float median = lengths[n_cells/2];
+
+  std::map<int, int> fine_to_coarse_vertex_map;
+
+  //Vertici da visitare
+  std::stack<int> to_visit_vertices;
+  std::stack<int> to_visit_short_vertices;
+
+  //Celle visitate
+  std::vector<bool> visited_cell(n_cells, false);
+
+  //Vettore che contiene true alla posizione i se l'i-esimo vertice
+  //è da collassare (Non so come altro fare, forse c'è un modo migliore)
+  std::vector<bool> is_collapsed(n_cells + 1, false);
+
+  //I vertici da collassare, raggruppati per componente connessa
+  std::vector<std::vector<int>> to_collapse_vertices;
+
+  //Quando entro in short mode, inizio a riempire questo vector
+  //Quando esco dalla short mode, lo pusho in to_collapse_vertices
+  std::vector<int> current_to_collapse_vertices_connected_component;
+
+  //Il primo vertice da visitare
+  to_visit_vertices.push(0);
+
+  bool short_mode = false;
+
+  int visiting_vertex;
+
+  while(to_visit_vertices.size() != 0) {
+
+    if (short_mode) {
+      visiting_vertex = to_visit_short_vertices.top();
+      to_visit_short_vertices.pop();
+    }
+    else {
+      visiting_vertex = to_visit_vertices.top();
+      to_visit_vertices.pop();
+    }
+    
+    //Se non sono in short mode, prima di esplorare controllo che almeno una cella sia corta,
+    //Altrimenti rischio di aggiungere celle non corte che vengono visitate prima di quelle corte
+    if (!short_mode) {
+
+      for (unsigned int to_visit_cell : adiacency[visiting_vertex]) {
+        if (!visited_cell[to_visit_cell]) {
+          int other_vertex_cell = cells[to_visit_cell].vertices[1]+cells[to_visit_cell].vertices[0] - visiting_vertex;
+
+          if ((points[other_vertex_cell] - points[visiting_vertex]).norm_square() < median) {
+            short_mode = true;
+            break;
+          }
+        }
+      }
+    }
+
+    //Metto il vertice in cui mi trovo adesso tra quelli da collassare
+    if (short_mode) {
+      current_to_collapse_vertices_connected_component.push_back(visiting_vertex);
+      is_collapsed[visiting_vertex] = true;
+    }
+
+    for (unsigned int to_visit_cell : adiacency[visiting_vertex]) {
+      int other_vertex_cell = cells[to_visit_cell].vertices[1]+cells[to_visit_cell].vertices[0] - visiting_vertex;
+
+      if (!visited_cell[to_visit_cell]) {
+
+        if (short_mode && (points[other_vertex_cell] - points[visiting_vertex]).norm_square() < median) {
+          to_visit_short_vertices.push(other_vertex_cell);
+        }
+        else {
+          to_visit_vertices.push(other_vertex_cell);
+        }
+
+        visited_cell[to_visit_cell] = true;
+      }
+    }
+
+    if (short_mode && to_visit_short_vertices.size() == 0) {
+      to_collapse_vertices.push_back(current_to_collapse_vertices_connected_component);
+      current_to_collapse_vertices_connected_component.clear();
+      short_mode = false;
+    }
+
+  }
+
+  //Creo i punti
+  {
+    for (unsigned int i = 0; i < n_cells + 1; i++) {
+      if (!is_collapsed[i]){
+        result.add_point(points[i]);
+
+        fine_to_coarse_vertex_map[i] = result.get_number_of_points() - 1;
+      } 
+    }
+
+    int old_vertices = fine_to_coarse_vertex_map.size();
+
+    for (unsigned int j = 0; j < to_collapse_vertices.size(); j++) {
+      auto connected_componet = to_collapse_vertices[j];
+
+      Point<3> new_vertex(0,0,0);
+
+      for (unsigned int i = 0; i < connected_componet.size(); i++) {
+        new_vertex += points[connected_componet[i]];
+
+        fine_to_coarse_vertex_map[connected_componet[i]] = old_vertices+j;
+      }
+
+      //Il punto in mezzo
+      new_vertex /= connected_componet.size();
+
+      result.add_point(new_vertex);
+    }
+  }
+  
+
+  //Creo le celle
+  {
+    for (const auto & cell : cells) 
+    {
+      if ( fine_to_coarse_vertex_map[cell.vertices[0]] != fine_to_coarse_vertex_map[cell.vertices[1]]) {
+        result.add_cell(fine_to_coarse_vertex_map[cell.vertices[0]], fine_to_coarse_vertex_map[cell.vertices[1]]);
+      }
+    }
+  }
+
+  return result;
+}
 
 class Poisson
 {
@@ -134,6 +355,10 @@ private:
 
   const PoissonParameters &par;
 
+  Triangulation<1,3> coarse_triangulation;
+  FE_Q<1,3> coarse_fe;
+  DoFHandler<1,3> coarse_dof_handler;
+
   Triangulation<1,3> triangulation;
   FE_Q<1,3>          fe;
   DoFHandler<1,3>    dof_handler;
@@ -148,18 +373,17 @@ private:
 };
 
 
-
-
 Poisson::Poisson(const PoissonParameters &par)
   : par(par)
   , fe(par.fe_degree)
+  , coarse_fe(par.fe_degree)
   , dof_handler(triangulation)
+  , coarse_dof_handler(coarse_triangulation)
 {}
-
 
 void
 Poisson::make_grid()
-{
+{  
   std::ifstream in("mesh.vtk");
 
   //Contiene ciò che devo leggere dal file
@@ -175,7 +399,8 @@ Poisson::make_grid()
   //Numero di punti
   in >> buffer;
   int n_points = std::stoi(buffer);
-  std::vector<Point<3>> points(n_points);
+
+  Graph fine_graph;
 
   //Il tipo della variabile, non ci serve
   in >> buffer;
@@ -194,7 +419,7 @@ Poisson::make_grid()
     in >> buffer;
     z = std::stod(buffer);
 
-    points[i] = Point<3>(x,y,z);
+    fine_graph.add_point(x, y, z);
   }
 
   //Per ora ho cancellato dalla mesh l'offset, poi vedo
@@ -204,18 +429,14 @@ Poisson::make_grid()
 
   //Il numero di celle
   in >> buffer;
-  int n_cells = std::stoi(buffer);
-
+  unsigned int n_cells = std::stoi(buffer) - 1;
 
   //Parole che non ci servono
   for (int i = 0; i < 3; i++)
     in >> buffer;
 
-  std::vector<std::array<int,GeometryInfo<1>::vertices_per_cell>>
-    cell_vertices;
-
   int node1, node2;
-  for (int i = 0; i < 2 * (n_cells-1); i++) {
+  for (int i = 0; i < 2 * n_cells; i++) {
     in >> buffer;
     node1 = std::stoi(buffer);
 
@@ -224,20 +445,27 @@ Poisson::make_grid()
     in >> buffer;
     node2 = std::stoi(buffer);
 
-    cell_vertices.push_back({node1, node2});
+    fine_graph.add_cell(node1, node2);
   }
-  
 
-   std::vector<CellData<1>> cells(cell_vertices.size(), CellData<1>());
-  for (unsigned int i=0; i<cell_vertices.size(); ++i)
-    for (unsigned int j=0; j<GeometryInfo<1>::vertices_per_cell; ++j)
-      cells[i].vertices[j] = cell_vertices[i][j];
+  triangulation = fine_graph.create_graph_triangulation();
+  coarse_triangulation = fine_graph.get_coarser_graph().create_graph_triangulation();
 
-  triangulation.create_triangulation (points, cells, SubCellData());
+  GridOut grid_out;
+
+  std::ofstream out("n_coarse" + std::to_string(0) + ".vtk");
+  grid_out.write_vtk(coarse_triangulation, out);
+  out.close();
+
+  std::ofstream out2("fine" + std::to_string(0) + ".vtk");
+  grid_out.write_vtk(triangulation, out2);
+  out2.close();
 
   std::cout << "   Number of active cells: " << triangulation.n_active_cells()
             << std::endl
             << "   Total number of cells: " << triangulation.n_cells()
+            << std::endl
+            << "   Total number of points: " << triangulation.n_faces()
             << std::endl;
 }
 
@@ -276,9 +504,6 @@ Poisson::setup_system()
   solution.reinit(dof_handler.n_dofs());
   system_rhs.reinit(dof_handler.n_dofs());
 }
-
-
-
 
 void
 Poisson::assemble_system()
@@ -325,7 +550,6 @@ Poisson::assemble_system()
 
 }
 
-
 void
 Poisson::solve()
 {
@@ -336,10 +560,31 @@ Poisson::solve()
 
   std::cout << "   " << solver_control.last_step()
             << " CG iterations needed to obtain convergence." << std::endl;
+  
+
+  //Restriction and prolongation
+
+  /*
+  auto a_fine = linear_operator(system_matrix);
+  auto id_fine = identity_operator(a_fine);
+
+  auto restriction = id_fine;
+  restriction.vmult = [&](Vector<double> &dst, const Vector<double> &src) {
+
+  };
+  restriction.Tvmult = [&](Vector<double> &dst, const Vector<double> &src) {
+
+  };
+
+  restriction.reinit_range_vector = [&](Vector<double> &v, bool) {
+    v.reinit(coarse_dof_handler.n_dofs());
+  };
+  restriction.reinit_domain_vector = [&](Vector<double> &v, bool) {
+    v.reinit(dof_handler.n_dofs());
+  };
+
+  */
 }
-
-
-
 
 void
 Poisson::output_results(const unsigned int cycle) const
@@ -364,9 +609,6 @@ Poisson::output_results(const unsigned int cycle) const
 
   DataOutBase::write_pvd_record(pvd_output, times_and_names);
 }
-
-
-
 
 void
 Poisson::run()
